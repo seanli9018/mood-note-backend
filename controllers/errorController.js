a1635d81ef9f8596e95736eb7f0c5ca40900172c
seanli9018 <seanli9018@gmail.com>
2025-03-15 09:59:30 -0500

    refined error handling: uncaught exception and unhandled rejection

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicatedErrorDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((errItem) => errItem.message);
+  const message = `Invalid input data. ${errors.join('; ')}`;
+  return new AppError(message, 400);
+};
+
 // send error while in development env.
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -25,6 +44,7 @@ const sendErrorProd = (err, res) => {
   });
 };
 
+// Global error handler: err always as the first params, and it will receive errors from all parts of the App that invoke next(err)
 module.exports = (err, _req, res, _next) => {
   err.statusCode = err.statusCode ?? 500;
   err.status = err.status ?? 'error';
@@ -34,6 +54,17 @@ module.exports = (err, _req, res, _next) => {
   }
 
   if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = JSON.parse(JSON.stringify(err));
+
+    // Convert mongoose errors to operational error with user friendly msg.
+    // example: _id: random_string which is not compliant with mongo DB ObjectId.
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // duplicated fields, if certain field must be unique.
+    if (error.code === 11000) error = handleDuplicatedErrorDB(error);
+    // field validation errors.
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+// Globally handles all Uncaught Exception!!!!!
+// Uncaught Exception is sync error so that we have to put it on the very beginning of the code.
+// example: console.log(x); Reference Error x is not defined!!!
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  // Shutting down directly, since if a uncaught exception is happening, Node.js server is in a unclean state.
+  // In real production env, the host may have logic to restart the server immediately.
+  process.exit(1);
+});
+
 // Injecting env variable.
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -15,6 +27,18 @@ mongoose.connect(DB_CONNECTION_STRING).then((_con) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+// Globally handles all Unhandled Rejections!!!!!
+// example: DB connection errors
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  // Shutting down gracefully by calling server.close first.
+  // We are giving time for server to finish all pending requests, then shutting it down.
+  server.close(() => {
+    process.exit(1);
+  });
 });
